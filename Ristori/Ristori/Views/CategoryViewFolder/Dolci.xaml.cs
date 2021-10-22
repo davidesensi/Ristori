3440103aa21d9bf9bd52f11f4ebed3eb5d139787
ZappaSte <ste.zappasodi@gmail.com>
2021-10-22 10:19:53 +0200

    0.3.5 Aggiunte dei Prodotti Menu mancanti

---
+++ Ristori/Ristori/Views/CategoryViewFolder/Dolci.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Ristori.Models;
+using Ristori.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ristori.Views.CategoryViewFolder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Dolci : ContentPage
+    {
+
+        CategoryViewModel cvm;
+        public Dolci()
+        {
+            InitializeComponent();
+            int ID = new int();
+            ID = 4;
+            cvm = new CategoryViewModel(ID);
+            this.BindingContext = cvm;
+        }
+
+        async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedProduct = e.CurrentSelection.FirstOrDefault() as Product;
+            if (selectedProduct == null)
+                return;
+            await Navigation.PushModalAsync(new ProductDetailsView(selectedProduct));
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
+}
---
+++ Ristori/Ristori/Views/CategoryViewFolder/PizzeBianche.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Ristori.Models;
+using Ristori.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ristori.Views.CategoryViewFolder
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PizzeBianche : ContentPage
+    {
+
+        CategoryViewModel cvm;
+        public PizzeBianche()
+        {
+            InitializeComponent();
+            int ID = new int();
+            ID = 3;
+            cvm = new CategoryViewModel(ID);
+            this.BindingContext = cvm;
+        }
+
+        async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedProduct = e.CurrentSelection.FirstOrDefault() as Product;
+            if (selectedProduct == null)
+                return;
+            await Navigation.PushModalAsync(new ProductDetailsView(selectedProduct));
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
+}
