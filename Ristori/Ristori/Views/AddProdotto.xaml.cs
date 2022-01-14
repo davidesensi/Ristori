7167c81433f4859973ad75532bb8549b9d8bca0b
ZappaSte <ste.zappasodi@gmail.com>
2022-01-14 16:14:46 +0100

    0.5.6.2 Piccola modifica bottone conferma ordine

---
+++ Ristori/Ristori/Views/AddProdotto.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Ristori.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ristori.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddProdotto : ContentPage
+    {
+        public AddProdotto()
+        {
+            InitializeComponent();
+            this.BindingContext = new ProductsViewModel();
+            base.OnAppearing();
+        }
+        protected override void OnAppearing()
+        {
+            this.BindingContext = new ProductsViewModel();
+            base.OnAppearing();
+        }
+        async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
